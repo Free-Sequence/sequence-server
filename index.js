53a7d2a1e61452611af395e7b06f3111b341a6a5
Harsh Shah <harsh.shah000@gmail.com>
2020-04-05 23:19:35 -0400

    ESLint Integration

--- index.js
+++ index.js
@@ -4,20 +4,8 @@ const app = express();
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 
-// const { setHeaders } = require('./middlewares/setHeaders.middleware');
-// const CORSMiddleware = require('./middlewares/CORS.middleware');
-// const { limiter } = require('./middlewares/ratelimiter.middleware');
-
-// app.use(CORSMiddleware);
 app.use(helmet());
-// app.use(setHeaders);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// const { login } = require('./routes/login/login.route');
-
-// app.post('/api/login', limiter(100, 15 * 60 * 1000), (req, res) => {
-//   login(req, res);
-// });
 
 module.exports = { app };
